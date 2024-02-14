bd233179479dccc2d778277a275eafccade25c79
Khánh Long Trần <khanhtl2003gttn@gmail.com>
2024-02-14 22:21:10 +0700

    Support static file. Using abstract factory pattern.

---
+++ TinyWebServer.Server/StaticFileSupport/StaticFileCallable.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TinyWebServer.Abstractions;
+using TinyWebServer.Abstractions.Http;
+using TinyWebServer.Server.Http.Helpers;
+
+namespace TinyWebServer.Server.StaticFileSupport
+{
+
+    // Build response builder object if url exists. If not, response is not found state
+    public class StaticFileCallable : ICallableResource
+    {
+        private readonly FileInfo file;
+        public StaticFileCallable(FileInfo file)
+        {
+            this.file = file;
+        }
+        public async Task OnGet(HttpRequest request, IHttpResponseBuilder responseObjectBuilder)
+        {
+            if (file.Exists)
+            {
+                try
+                {
+                    // Create stream reader to read file
+                    using (var reader = file.OpenText())
+                    {
+                        var contentFile = await reader.ReadToEndAsync();
+
+                        responseObjectBuilder.AddHeader("Content-type", "text/html");
+                        responseObjectBuilder.AddHeader("Content-length", contentFile.Length.ToString());
+                        responseObjectBuilder.SetContent(new Http.Content.StringContent(contentFile));
+
+                        StandardResponseBuilderHelpers.Ok(responseObjectBuilder);
+
+                    }
+                } catch (Exception ex)
+                {
+                    StandardResponseBuilderHelpers.NotFound(responseObjectBuilder);
+                }
+
+            }
+        }
+    }
+}
---
+++ TinyWebServer.Server/StaticFileSupport/StaticFileRoutingService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TinyWebServer.Abstractions;
+
+namespace TinyWebServer.Server.StaticFileSupport
+{
+    /// <summary>
+    /// Concrete product (implement abstract product)
+    /// </summary>
+    public class StaticFileRoutingService : IRoutingService
+    {
+        private readonly DirectoryInfo directoryInfo;
+        public StaticFileRoutingService(DirectoryInfo directoryInfo)
+        {
+            this.directoryInfo = directoryInfo ?? throw new ArgumentNullException(nameof(directoryInfo));
+        }
+
+        public ICallableResource? FindRoute(string url)
+        {
+            if (url == null)
+                throw new ArgumentNullException(nameof(url));
+            if (directoryInfo.Exists)
+            {
+                if (url.StartsWith("/"))
+                {
+                    url = url[1..];
+                }
+                url = url.Replace('/', Path.DirectorySeparatorChar);
+
+                // Combine directory to this folder and this file in this folder (url)
+                var file = new FileInfo(Path.Combine(directoryInfo.FullName, url));
+                if (file.Exists)
+                {
+                    return new StaticFileCallable(file);
+                }
+            }
+            return null;
+        }
+    }
+}
