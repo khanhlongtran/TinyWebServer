e2ff24d560ebca08c4733ddd5f4a2b115dbe80c6
Khánh Long Trần <khanhtl2003gttn@gmail.com>
2024-02-14 14:31:35 +0700

    Create some main object: Request, Response, Server in Abstractions project. In this project, this will define some interface (contract) as well as design pattern (builder pattern and factory pattern, maybe singleton pattern) to create object.

---
+++ TinyWebServer.Abstractions/IProtocolHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using TinyWebServer.Abstractions.Http;
+
+namespace TinyWebServer.Abstractions
+{
+    public interface IProtocolHandler
+    {
+        int ProtocolVersion
+        {
+            get;
+        }
+        Task<BuildRequestState> ReadRequest(TcpClient tcpClient, IHttpRequestBuilder requestBuilder, ProtocolHandlerData data);
+        Task SendResponse(TcpClient tcpClient, IHttpResponseBuilder responseBuilder, ProtoconHandlerData data);
+    }
+}
---
+++ TinyWebServer.Abstractions/IServer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinyWebServer.Abstractions
+{
+    public interface IServer : IDisposable
+    {
+        void Start();
+        void Stop();
+    }
+}
